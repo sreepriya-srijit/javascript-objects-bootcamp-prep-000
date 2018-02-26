b86c3d2e34b76c93ce652fd6a76ff8398876c0e2
Sreepriya Srijit <sreepriyakurup87@gmail.com>
2018-02-26 23:17:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {Kanye: "Gold Digger"}
 function updatePlaylist(playlist, artistName, songTitle){
   return Object.assign({},playlist,{[artistName] : songTitle})
 }
-
+console.log(playlist)
 function removeFromPlaylist(playlist, artistName){
   delete playlist.artistName
   return playlist
