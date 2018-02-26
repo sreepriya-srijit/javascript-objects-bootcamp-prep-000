6b9e14fbcffaecc22cd79618bcafb03bdaea6d76
Sreepriya Srijit <sreepriyakurup87@gmail.com>
2018-02-26 23:15:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {'arti1' : 'player1', 'arti2' : 'player2'}
 
-function updatePlaylist(playlist, artistNa, songTitle){
-  return Object.assign({},playlist,{artistN : songTitle})
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign({},playlist,{[artistName] : songTitle})
 }
 
 function removeFromPlaylist(playlist, artistName){
